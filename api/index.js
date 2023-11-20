62812bedba184c3bccca8b95de2fe08e47611f56
larreaf <fedeml.95@gmail.com>
2023-11-19 22:27:17 -0300

    Added cors origin env

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: process.env.CORS_ORIGIN
 }))
 
 app.get('/ranking', async (req, res) => {
