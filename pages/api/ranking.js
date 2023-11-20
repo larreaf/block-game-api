01c4051ca6dfc280a94d275627bf69fb915f149f
larreaf <fedeml.95@gmail.com>
2023-11-20 00:46:53 -0300

    Added post ranking endpoint

--- pages/api/ranking.js
+++ pages/api/ranking.js
@@ -1,24 +1,59 @@
 import { createClient } from '@vercel/kv';
 
 
-export default async function handler(req, res) {
+const getRanking = async (req, res) => {
     const blockGameRedis = createClient({
         url: process.env.KV_REST_API_URL,
         token: process.env.KV_REST_API_TOKEN,
     });
-    
-    const rankingArray = await blockGameRedis.zrange('ranking', 0, 9, {withScores: true, rev: true});
+
+    const rankingArray = await blockGameRedis.zrange('ranking', 0, 9, { withScores: true, rev: true });
 
     const ranking = []
 
     for (let i = 0; i < rankingArray.length; i += 2) {
-    const name = rankingArray[i];
-    const score = rankingArray[i + 1];
-    ranking.push({ name, score });
+        const name = rankingArray[i];
+        const score = rankingArray[i + 1];
+        ranking.push({ name, score });
     }
 
     res.statusCode = 200;
     res.setHeader("Access-Control-Allow-Origin", "https://block-game-kappa.vercel.app");
     res.setHeader('Content-Type', 'application/json');
     res.json(ranking);
+}
+
+const postRanking = async (req, res) => {
+    const blockGameRedis = createClient({
+        url: process.env.KV_REST_API_URL,
+        token: process.env.KV_REST_API_TOKEN,
+    });
+    console.log(req.body)
+    // const body = JSON.parse(req.body);
+
+    const name = req.body['name']
+
+    const score = req.body['score']
+
+    const result = await blockGameRedis.zadd('ranking', { score: score, member: name })
+
+    res.statusCode = 201
+    res.json()
+}
+
+const methods = {
+    'GET': getRanking,
+    'POST': postRanking
+}
+
+export default async function handler(req, res) {
+    const methodHandler = methods[req.method]
+
+    if (methodHandler === null || typeof (methodHandler) === 'undefined') {
+        res.statusCode = 404
+        res.json()
+        return;
+    }
+
+    await methodHandler(req, res);
 }
